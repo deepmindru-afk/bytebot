9367b5e5483f760cc7c85f211a3e6518653f6574
deepmindru-afk <deepmindru@gmail.com>
2025-09-09 20:55:09 +0300

    Update Header.tsx

--- packages/bytebot-ui/src/components/layout/Header.tsx
+++ packages/bytebot-ui/src/components/layout/Header.tsx
@@ -78,10 +78,10 @@ export function Header() {
             <span className="text-sm">Desktop</span>
           </Link>
           <Link
-            href="https://docs.bytebot.ai/quickstart"
+            href="https://www.portalos.ru"
             target="_blank"
             rel="noopener noreferrer"
-            className={getLinkClasses("https://docs.bytebot.ai")}
+            className={getLinkClasses("https://www.portalos.ru")}
           >
             <HugeiconsIcon icon={DocumentCodeIcon} className="h-4 w-4" />
             <span className="text-sm">Docs</span>
